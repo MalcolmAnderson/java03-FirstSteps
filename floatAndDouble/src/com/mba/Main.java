5636b7650a28972c31c8159167a96d170600359a
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2020-07-18 23:17:25 -0700

    complete section 03-28

---
+++ floatAndDouble/src/com/mba/Main.java
@@ -0,0 +1,21 @@
+package com.mba;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    float myMinFloatVal = Float.MIN_VALUE;
+	    float myMaxFloatVal = Float.MAX_VALUE;
+        System.out.println("Float min value = " + myMinFloatVal);
+        System.out.println("Float max value =  " + myMaxFloatVal);
+
+        double myMinDoubleVal = Double.MIN_VALUE;
+        double myMaxDoubleVal = Double.MAX_VALUE;
+        System.out.println("Double min value = " + myMinDoubleVal);
+        System.out.println("Double max value =  " + myMaxDoubleVal);
+
+        int myIntValue = 5;
+        float myFloatValue = (float)5.25;
+        double myDoubleValue = 5d;
+
+    }
+}
